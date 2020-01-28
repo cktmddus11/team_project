ae83401e9e1186ee0ba43899a347f61087975394
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2020-01-28 16:02:58 +0900

    cyj_test
    cyj_test

---
+++ src/main/java/controller/Admin_InController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin_in")
+public class Admin_InController {
+	@GetMapping("*") // get���� ���� ��û�� �������� ������ �̰ɷ� ����?
+	public String form(Model model) {
+		return null; // null : url��  ���� �̵�?
+	}
+}
--- src/main/java/controller/KakaoController.java
+++ src/main/java/controller/KakaoController.java
@@ -35,7 +35,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import logic.Kakaoupdate;
+
 
 public class KakaoController {
 
